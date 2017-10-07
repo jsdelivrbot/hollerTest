bf2c073a93a65e5e42fd038c6fc5e1ee643693dc
William Dunlop <william.dunlop.1993@gmail.com>
2017-10-07 16:53:48 +1100

    improved and fixed errors for form feedback

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -9,10 +9,13 @@ export default class Login extends React.Component {
       username: '',
       password: '',
       usernameError: '',
-      passwordError: ''
+      passwordError: '',
+      formIsValid: true,
+      formFeedback: ''
     }
 
     this.loginForm = this.loginForm.bind(this)
+    this._loginFormIsValid = this._loginFormIsValid.bind(this)
   }
 
   showForm() {
@@ -47,63 +50,76 @@ export default class Login extends React.Component {
         <div className={this.state.passwordError ? 'validation-feedback' : 'hidden'}>
           <em>{this.state.passwordError}</em>
         </div>
-        <div className={this.state.formError ? 'validation-feedback' : 'hidden'}>
-          <em>{this.state.formError}</em>
+        <div className={this.state.formIsValid ? 'success-feedback' : 'validation-feedback'}>
+          <em>{this.state.formFeedback}</em>
         </div>
 
       </div>
     )
   }
 
-  // validateLoginForm() {
-  //   if(this._validateUsername() )
-  // }
-
-  _validateUsername() {
-    if (this.state.username.length > 3) {
-      this.setState({ usernameError: ''})
-    } else {
-      this.setState({ usernameError: 'Username must contain at least 4 characters'})
-      return false;
-    }
-    // return this.state.username.length > 3;
+  submitLoginForm() {
+    this._resetFormFeedback();
+    // 500ms timer to simulate API call
+    setTimeout(
+      this._loginFormIsValid()
+    , 500);
   }
 
-  _validatePassword() {
-    //  format username for comparisson
-    const reversedUsername = this.state.username.split("").reverse().join("")
-    const alphanumericReversedUsername = reversedUsername.replace(/[^a-z0-9]+/gi, '');
+  _resetFormFeedback() {
+    this.setState({
+      formIsValid: false,
+      formFeedback: ''
+    })
+  }
 
-    // const passwordLengthValidation = this.state.password.length > 3;
-    // const passwordContainsNumOrSym = /.*[0-9!@#$%^&*()=+\-_,.;'\[\]<>?:"|{}/\\]/.test(this.state.password)
-
-    if(this._validatePasswordLength() && this._validatePasswordCharacters()) {
+  _loginFormIsValid() {
+    if(this._validateUsername() && this._validatePassword()) {
+      //  format username for comparisson
+      const reversedUsername = this.state.username.split("").reverse().join("")
+      const alphanumericReversedUsername = reversedUsername.replace(/[^a-z0-9]+/gi, '');
+      //  format password for comparisson
       const alphanumericPassword = this.state.password.replace(/[^a-z0-9]+/gi, '')
       const isAlphanumericallyMatched = alphanumericPassword.toLowerCase() === alphanumericReversedUsername.toLowerCase();
 
       if(isAlphanumericallyMatched) {
-        this.setState({ passwordError: '' })
+        this.setState({
+          formIsValid: true,
+          formFeedback: 'Success'
+        })
         return true;
       } else {
-        this.setState({ passwordError: 'The username and password do not match' });
+        this.setState({
+          formIsValid: false,
+          formFeedback:  'The username and password do not match'
+        });
         return false;
       }
     }
+  }
+  _validateUsername() {
+    if (this.state.username.length > 3) {
+      this.setState({ usernameError: ''})
+      return true;
+    }
+    this.setState({ usernameError: 'Username must contain at least 4 characters'})
+    return false;
+  }
 
-
-    // console.log('isAlphanumericallyMatched', isAlphanumericallyMatched)
-    // console.log('passwordMeetsRequirements', passwordMeetsRequirements)
-
+  _validatePassword() {
+    if(this._validatePasswordLength() && this._validatePasswordCharacters()) {
+      return true;
+    }
+    return false;
   }
 
   _validatePasswordLength() {
     if(this.state.password.length > 3) {
       this.setState({ passwordError: ''});
       return true;
-    } else {
-      this.setState({ passwordError: 'Password must contain at least 4 characters' })
-      return false;
     }
+    this.setState({ passwordError: 'Password must contain at least 4 characters' })
+    return false;
   }
 
   _validatePasswordCharacters() {
@@ -118,15 +134,7 @@ export default class Login extends React.Component {
   }
 
 
-  submitLoginForm() {
-    // this.validateLoginForm()
-    // console.log('username valid?', this._validateUsername())
-    // console.log('password valid?', this._validatePassword())
-    this._validateUsername();
-    this._validatePassword();
-    // console.log('submission valid?', this._validateUsername() && this._validatePassword())
 
-  }
 
   loginButtonControl() {
     if (!this.state.displayForm) {
