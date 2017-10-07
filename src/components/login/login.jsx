ece4e9a45cb028f087803f9a370d15d8bd4e46c6
William Dunlop <william.dunlop.1993@gmail.com>
2017-10-07 17:33:31 +1100

    eslint was installed and linting was applied

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -3,7 +3,7 @@ import './login.css';
 
 export default class Login extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       displayForm: false,
       username: '',
@@ -11,41 +11,40 @@ export default class Login extends React.Component {
       usernameError: '',
       passwordError: '',
       formIsValid: true,
-      formFeedback: ''
-    }
-
-    this.loginForm = this.loginForm.bind(this)
-    this._loginFormIsValid = this._loginFormIsValid.bind(this)
+      formFeedback: '',
+      loading: false,
+    };
+    this._loginForm = this._loginForm.bind(this);
+    this._loginFormIsValid = this._loginFormIsValid.bind(this);
   }
 
-  showForm() {
+  _showForm() {
     this.setState({ displayForm: true });
   }
 
   _handleChange(value, origin) {
-    this.setState({ [origin]: value})
+    this.setState({ [origin]: value });
   }
 
-  loginForm() {
-    const displayForm = this.state.displayForm;
+  _loginForm() {
     return (
-      <div className={displayForm ? 'displayed-form' : 'hidden-form'}>
+      <div className={this.state.displayForm ? 'displayed-form' : 'hidden-form'}>
         <input
-          type={displayForm ? 'text' : 'hidden'}
+          type={this.state.displayForm ? 'text' : 'hidden'}
           placeholder="Username"
           className="text-field"
           value={this.state.username}
-          onChange={e => this._handleChange(e.target.value, "username")}
+          onChange={e => this._handleChange(e.target.value, 'username')}
         />
         <div className={this.state.usernameError ? 'validation-feedback' : 'hidden'}>
           <em>{this.state.usernameError}</em>
         </div>
         <input
-          type={displayForm ? 'text' : 'hidden'}
+          type={this.state.displayForm ? 'text' : 'hidden'}
           placeholder="Password"
           className="text-field"
           value={this.state.password}
-          onChange={e => this._handleChange(e.target.value, "password")}
+          onChange={e => this._handleChange(e.target.value, 'password')}
         />
         <div className={this.state.passwordError ? 'validation-feedback' : 'hidden'}>
           <em>{this.state.passwordError}</em>
@@ -55,92 +54,90 @@ export default class Login extends React.Component {
         </div>
 
       </div>
-    )
+    );
   }
 
-  submitLoginForm() {
-    this._resetFormFeedback();
+  _submitLoginForm() {
+    this.setState({ loading: true });
     // 500ms timer to simulate API call
-    setTimeout(
-      this._loginFormIsValid()
-    , 500);
+    setTimeout(() => {
+      this._resetFormFeedback();
+      this._loginFormIsValid();
+    }, 500);
   }
 
   _resetFormFeedback() {
     this.setState({
       formIsValid: false,
-      formFeedback: ''
-    })
+      formFeedback: '',
+      loading: false,
+    });
   }
 
   _loginFormIsValid() {
-    if(this._validateUsername() && this._validatePassword()) {
+    if (this._validateUsername() && this._validatePassword()) {
       //  format username for comparisson
-      const reversedUsername = this.state.username.split("").reverse().join("")
+      const reversedUsername = this.state.username.split('').reverse().join('');
       const alphanumericReversedUsername = reversedUsername.replace(/[^a-z0-9]+/gi, '');
       //  format password for comparisson
-      const alphanumericPassword = this.state.password.replace(/[^a-z0-9]+/gi, '')
-      const isAlphanumericallyMatched = alphanumericPassword.toLowerCase() === alphanumericReversedUsername.toLowerCase();
+      const alphanumericPassword = this.state.password.replace(/[^a-z0-9]+/gi, '');
+      const isAlphanumericallyMatched =
+        alphanumericPassword.toLowerCase() === alphanumericReversedUsername.toLowerCase();
 
-      if(isAlphanumericallyMatched) {
+      if (isAlphanumericallyMatched) {
         this.setState({
           formIsValid: true,
-          formFeedback: 'Success'
-        })
-        return true;
-      } else {
-        this.setState({
-          formIsValid: false,
-          formFeedback:  'The username and password do not match'
+          formFeedback: 'Success',
         });
-        return false;
+        return true;
       }
+      this.setState({
+        formIsValid: false,
+        formFeedback: 'The username and password do not match',
+      });
+      return false;
     }
+    return false;
   }
   _validateUsername() {
     if (this.state.username.length > 3) {
-      this.setState({ usernameError: ''})
+      this.setState({ usernameError: '' });
       return true;
     }
-    this.setState({ usernameError: 'Username must contain at least 4 characters'})
+    this.setState({ usernameError: 'Username must contain at least 4 characters' });
     return false;
   }
 
   _validatePassword() {
-    if(this._validatePasswordLength() && this._validatePasswordCharacters()) {
+    if (this._validatePasswordLength() && this._validatePasswordCharacters()) {
       return true;
     }
     return false;
   }
 
   _validatePasswordLength() {
-    if(this.state.password.length > 3) {
-      this.setState({ passwordError: ''});
+    if (this.state.password.length > 3) {
+      this.setState({ passwordError: '' });
       return true;
     }
-    this.setState({ passwordError: 'Password must contain at least 4 characters' })
+    this.setState({ passwordError: 'Password must contain at least 4 characters' });
     return false;
   }
 
   _validatePasswordCharacters() {
-    if(/.*[0-9!@#$%^&*()=+\-_,.;'\[\]<>?:"|{}/\\]/.test(this.state.password)) {
-      this.setState({ passwordError: ''})
+    if (/.*[0-9!@#$%^&*()=+\-_,.;'[\]<>?:"|{}/\\]/.test(this.state.password)) {
+      this.setState({ passwordError: '' });
       return true;
-    } else {
-      this.setState({ passwordError: 'Password must contain one number or symbol' })
-      console.log('Password does not have chars')
-      return false;
     }
+    this.setState({ passwordError: 'Password must contain one number or symbol' });
+    return false;
   }
 
-
-
-
-  loginButtonControl() {
+  _loginButtonControl() {
     if (!this.state.displayForm) {
-      this.showForm();
+      this._showForm();
     } else {
-      this.submitLoginForm()
+      this._submitLoginForm();
     }
   }
 
@@ -150,14 +147,15 @@ export default class Login extends React.Component {
         <div className="login-container">
           <h3>Holler Frontend Technical Test</h3>
           <hr />
-          {this.loginForm()}
+          {this._loginForm()}
           <div className="button-container">
             <button
-              className="btn btn-lg btn-primary"
-              onClick={() => this.loginButtonControl()}
-              >
-                Login
-              </button>
+              className="btn btn-lg btn-primary login-button"
+              onClick={() => this._loginButtonControl()}
+            >
+              {!this.state.loading && 'Login'}
+              <i className={this.state.loading && 'fa fa-spinner fa-spin'} />
+            </button>
           </div>
         </div>
       </div>
